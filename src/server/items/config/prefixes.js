d2a84cd837b8f2a44b217bf8ac2a273f3afa103d
Big Bad Waffle <skichenbrand@gmail.com>
2017-06-10 22:09:31 +0200

    first commit

---
+++ src/server/items/config/prefixes.js
@@ -0,0 +1,92 @@
+define([
+	
+], function(
+	
+) {
+	return {
+		generic: [
+			'insolent%',
+			'deviant%',
+			'citadel%',
+			'shadow%',
+			'thief%',
+			'denial%',
+			'glory%',
+			'prophecy%',
+			'knight%',
+			'courier%',
+			'doom%',
+			'reflective',
+			'sly',
+			'astute',
+			'grisly',
+			'dream',
+			'antique',
+			'royalty%',
+			'hunter%',
+			'wisdom%',
+			'defiant',
+			'keen',
+			'hell%',
+			'battle',
+			'majesty',
+			'barbaric',
+			'palace',
+			'biting',
+			'scorpion%',
+			'partisan',
+			'foe%',
+			'union',
+			'command',
+			'sanguine',
+			'veteran%',
+			'faith%',
+			'avarice',
+			'vengeance',
+			'hideous',
+			'guru%',
+			'archer%',
+			'cavalry',
+			'tower',
+			'eternity',
+			'gruesome',
+			'sergeant%',
+			'demolisher%',
+			'virtue',
+			'riveted',
+			'sky',
+			'diamond',
+			'savant%',
+			'spite',
+			'shrewd',
+			'awful',
+			'master%',
+			'scout%'
+		],
+		armor: [
+			'unyielding',
+			'phalanx',
+		],
+		weapons: [
+			'biting',
+			'forge',
+			'piercing',
+			'steady'
+		],
+		slots: {
+			feet: [
+				'trail',
+				'kick'
+			],
+			hands: [
+				'knuckle',
+				'hand',
+				'pummel',
+				'grip'
+			],
+			waist: [
+				'sheath'
+			]
+		}
+	};
+});
---
+++ src/server/items/config/suffixes.js
@@ -0,0 +1,92 @@
+define([
+	
+], function(
+	
+) {
+	return {
+		generic: [
+			'gear',
+			'punishment',
+			'bite',
+			'light',
+			'mercy',
+			'strategy',
+			'alacrity',
+			'radiance',
+			'promise',
+			'dispute',
+			'spoil',
+			'rager',
+			'scorn',
+			'knack',
+			'style',
+			'ascension',
+			'envy',
+			'flourish',
+			'rescue',
+			'sentry',
+			'rigor',
+			'defiance',
+			'revenge',
+			'ichor',
+			'testimony',
+			'arts',
+			'boon',
+			'flash',
+			'horror',
+			'myth',
+			'wisp',
+			'dawn',
+			'judgment',
+			'inception',
+			'keeper',
+			'doctrine'
+		],
+		armor: [
+			'barrier',
+			'protector',
+			'armor',
+			'cover'
+		],
+		jewelry: [
+
+		],
+		weapons: [
+			'reaper',
+			'slasher',
+			'crusher',
+			'smite',
+			'render',
+			'executor',
+			'bite',
+			'union',
+			'talon'
+		],
+		slots: {
+			feet: [
+				'crushers',
+				'hooves',
+				'stompers'
+			],
+			hands: [
+				'vices',
+				'restraints',
+				'hands'
+			],
+			head: [
+				'sentinel',
+				'crown',
+				'visor',
+				'crest'
+			],
+			waist: [
+				'sheath',
+				'brace'
+			],
+			chest: [
+				'vestments',
+				'heart'
+			]
+		}
+	};
+});
