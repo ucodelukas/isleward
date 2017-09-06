8fb84ef950102cee4995931fc109bdda23ed007b
Big Bad Waffle <skichenbrand@gmail.com>
2017-09-06 18:46:51 +0200

    changed time of fishing tournament

--- src/server/config/maps/tutorial/events/fishingTournament.js
+++ src/server/config/maps/tutorial/events/fishingTournament.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'Fishing Tournament',
 	description: `Catch the biggest Ancient Carp for a chance to win Angler's Marks. Speak with Angler Nayla for more info.`,
 	distance: -1,
-	cron: '* * * * *',
+	cron: '50 18 * * *',
 
 	notifications: [{
 		mark: 0,
