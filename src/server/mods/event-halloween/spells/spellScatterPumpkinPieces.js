5b265abd0572e5c3dc45c8e3743ece6398b251bf
Big Bad Waffle <skichenbrand@gmail.com>
2017-10-09 21:07:00 +0200

    pumpkinbility

--- src/server/mods/event-halloween/spells/spellScatterPumpkinPieces.js
+++ src/server/mods/event-halloween/spells/spellScatterPumpkinPieces.js
@@ -7,16 +7,9 @@ define([
 		type: 'pumpkinChunk',
 
 		collisionEnter: function (o) {
-			if (!o.aggro)
+			if (!o.player)
 				return;
 
-			var isPlayer = !!this.caster.player;
-			var isTargetPlayer = !!o.player;
-
-			if ((!this.caster.aggro.canAttack(o)) && (isPlayer == isTargetPlayer))
-				return;
-
-			this.contents.push(o);
 		}
 	};
 
@@ -26,24 +19,39 @@ define([
 		cdMax: 5,
 		manaCost: 0,
 
+		spread: 7,
 		range: 10,
+		speed: 700,
 
 		cast: function (action) {
-			this.shootChunk(action);
+			return this.shootChunk(action);
 		},
 
 		shootChunk: function (action) {
 			var obj = this.obj;
-			var speed = 150;
-			var target = action.target;
 
-			var ttl = (Math.sqrt(Math.pow(target.x - obj.x, 2) + Math.pow(target.y - obj.y, 2)) * speed) - 50;
+			var physics = obj.instance.physics;
+
+			var spread = this.spread;
+			var toX = obj.x + ~~(Math.random() * spread * 2) - spread;
+			var toY = obj.y + ~~(Math.random() * spread * 2) - spread;
+			var target = physics.getClosestPos(
+				obj.x,
+				obj.y,
+				toX,
+				toY
+			);
+
+			if (!target)
+				return false;
+
+			var ttl = (Math.sqrt(Math.pow(target.x - obj.x, 2) + Math.pow(target.y - obj.y, 2)) * this.speed) - 50;
 
 			var projectileConfig = {
 				caster: this.obj.id,
 				components: [{
 					idSource: this.obj.id,
-					idTarget: target.id,
+					target: target,
 					type: 'projectile',
 					ttl: ttl,
 					projectileOffset: null,
@@ -80,12 +88,28 @@ define([
 					type: 'attackAnimation',
 					layer: 'projectiles',
 					loop: -1,
-					row: 0,
-					col: 0
+					row: 2,
+					col: 4
 				}]
 			};
 
 			this.sendAnimation(projectileConfig);
+
+			this.queueCallback(this.createChunk.bind(this, target), ttl, null, target);
+
+			return true;
+		},
+
+		createChunk: function (pos) {
+			var obj = this.obj.instance.objects.buildObjects([{
+				sheetName: 'objects',
+				cell: 167,
+				x: pos.x,
+				y: pos.y,
+				properties: {
+					cpnPumpkinChunk: cpnPumpkinChunk
+				}
+			}]);
 		}
 	};
 });
