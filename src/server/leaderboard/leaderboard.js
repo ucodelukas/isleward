22e5f434876ec168d32db54cbc90ecc7a5f7d1b4
Big Bad Waffle <skichenbrand@gmail.com>
2018-03-30 11:43:34 +0200

    undid db stuff, we'll do that laaater

--- src/server/leaderboard/leaderboard.js
+++ src/server/leaderboard/leaderboard.js
@@ -1,108 +1,189 @@
 define([
-	'security/io',
-	'fixes/fixes'
+	'../security/io'
 ], function (
-	io,
-	fixes
+	io
 ) {
 	return {
-		init: function () {
+		list: [],
+		waiting: [],
+		loaded: false,
 
+		init: function () {
+			this.getList();
 		},
 
 		requestList: function (msg) {
 			var prophecyFilter = msg.data.prophecies;
 			var offset = msg.data.offset;
 
-			var query = {
-				field: 'playerLevels',
-				offset: offset,
-				limit: 10,
-				order: {
-					level: 'DESC'
-				},
-				callback: this.onGetList.bind(this, msg)
-			};
+			var result = this.list;
+			var length = result.length;
 
-			if (!prophecyFilter) {
-				query.filter = [];
+			if (prophecyFilter) {
+				var pLen = prophecyFilter.length;
 
-				prophecyFilter.forEach(function (p) {
-					query.filter.push({
-						field: 'prophecies',
-						operator: 'like',
-						value: `'%${p}%'`
+				result = result
+					.filter(function (r) {
+						var rProphecies = r.prophecies || [];
+
+						var match = true;
+						for (var i = 0; i < pLen; i++) {
+							if (!rProphecies.some(rp => rp == prophecyFilter[i])) {
+								match = false;
+								break;
+							}
+						}
+
+						return match;
 					});
-				});
+
+				length = result.length;
+
+				result = result
+					.filter(function (r, i) {
+						return (
+							(i >= offset) &&
+							(i < offset + 10)
+						);
+					});
 			}
-
-			io.get(query);
-		},
-
-		onGetList: function (msg, result) {
-			result = result || [];
-
-			result = result.map(function (r) {
-				return {
-					name: r.key,
-					level: r.level
-				};
-			});
 
 			msg.callback({
 				list: result,
-				length: 123
+				length: length
 			});
+		},
+
+		getList: function () {
+			io.get({
+				ent: 'list',
+				field: 'leaderboard',
+				callback: this.onGetList.bind(this)
+			});
+		},
+
+		onGetList: function (result) {
+			if (!result) {
+				var list = {
+					list: []
+				};
+
+				io.set({
+					ent: 'list',
+					field: 'leaderboard',
+					value: JSON.stringify(list)
+				});
+			} else
+				this.parseList(result);
+
+			this.loaded = true;
+		},
+
+		parseList: function (result) {
+			this.list = JSON.parse(result).list;
+
+			if (!(this.list instanceof Array))
+				this.list = [];
+
+			this.list.forEach(function (l) {
+				if (l.name.indexOf(`'`) > -1)
+					l.name = l.name.split(`'`).join('');
+			});
+
+			this.list.forEach(function (l) {
+				for (var i = 0; i < 10; i++) {
+					this.list.push(l);
+				}
+			}, this);
+
+			var doSave = false;
+
+			this.waiting.forEach(function (w) {
+				if (!this.list.some(l => l.name == w.name)) {
+					this.list.push(w);
+					doSave = true;
+				}
+			}, this);
+
+			if (doSave)
+				this.save();
+
+			this.waiting = [];
 		},
 
 		getLevel: function (name) {
-			var query = {
-				key: name,
-				field: 'playerLevels',
-				callback: this.onGetLevel.bind(this)
-			};
+			if (!this.list)
+				return null;
+
+			var result = this.list.find(l => (l.name == name));
+			if (result)
+				return result.level;
+			else
+				return null;
 		},
 
 		setLevel: function (name, level, prophecies) {
-			var value = JSON.stringify({
-				name: name,
-				level: level,
-				prophecies: prophecies
-			});
+			if (!this.list) {
+				this.waiting.push({
+					name: name,
+					level: level,
+					prophecies: prophecies
+				});
 
-			io.set({
-				ent: name,
-				field: 'playerLevels',
-				extraFields: {
-					level: level
-				},
-				value: value
-			});
+				return;
+			}
+
+			var exists = this.list.find(l => l.name == name);
+			if (exists) {
+				if (exists.level != level) {
+					exists.level = level;
+
+					this.save();
+				}
+			} else {
+				this.list.push({
+					name: name,
+					level: level,
+					prophecies: prophecies
+				});
+
+				this.save();
+			}
 		},
 
 		deleteCharacter: function (name) {
-			io.delete({
-				ent: name,
-				field: 'playerLevels'
-			});
+			this.list.spliceWhere(l => (l.name == name));
+			this.save();
 		},
 
 		killCharacter: function (name) {
-			var char = io.get({
-				ent: name,
-				field: 'playerLevels',
-				callback: this.doKillCharacter.bind(this, name)
-			});
-		},
-		doKillCharacter: function (name, result) {
-			var obj = JSON.parse(result);
-			obj.dead = true;
+			var character = this.list.find(l => (l.name == name));
+			if (!character)
+				return;
 
-			var value = JSON.stringify(obj);
+			character.dead = true;
+			this.save();
+		},
+
+		sort: function () {
+			this.list.sort(function (a, b) {
+				return (b.level - a.level);
+			}, this);
+		},
+
+		save: function () {
+			this.sort();
+
+			if (!this.loaded)
+				return;
+
+			var value = JSON.stringify({
+				list: this.list
+			});
 
 			io.set({
-				ent: name,
-				field: 'playerLevels',
+				ent: 'list',
+				field: 'leaderboard',
 				value: value
 			});
 		}
--- src/server/security/io.js
+++ src/server/security/io.js
@@ -14,19 +14,11 @@ define([
 			stash: null,
 			skins: null,
 			login: null,
-			playerLevels: null,
+			leaderboard: null,
 			customMap: null,
 			mail: null,
 			customChannels: null,
 			error: null
-		},
-
-		tableKeys: {
-			playerLevels: {
-				key: 'VARCHAR(50)',
-				level: 'INT',
-				value: 'TEXT'
-			}
 		},
 
 		init: function (cbReady) {
@@ -41,18 +33,8 @@ define([
 			db.serialize(function () {
 				for (var t in tables) {
 
-					var fields = `key VARCHAR(50), value TEXT`;
-					var keys = scope.tableKeys[t];
-					if (keys) {
-						fields = '';
-						for (var k in keys) {
-							fields += `${k} ${keys[k]},`;
-						}
-						fields = fields.substr(0, fields.length - 1);
-					}
-
 					db.run(`
-						CREATE TABLE ${t} (${fields})
+						CREATE TABLE ${t} (key VARCHAR(50), value TEXT)
 					`, scope.onTableCreated.bind(scope));
 				}
 
@@ -70,67 +52,7 @@ define([
 			var key = options.ent;
 			var table = options.field;
 
-			var query = `SELECT * FROM ${table}`;
-
-			var filter = options.filter;
-			if (filter) {
-				query += ` WHERE`;
-
-				filter.forEach(function (f) {
-					query += ` ${f.field} ${f.operator} ${value}`;
-				});
-			} else if (key) {
-				query += ` WHERE key = '${key}'`;
-			}
-
-			var order = options.order;
-			if (order) {
-				query += ` ORDER BY`;
-
-				for (var p in order) {
-					query += ` ${p} ${order[p]}`;
-				}
-			}
-
-			query += ` LIMIT ${options.limit || 1}`;
-
-			var offset = options.offset;
-			if (offset)
-				query += ` OFFSET ${offset}`;
-
-			options.query = query;
-
-			if (key)
-				this.db.get(options.query, this.done.bind(this, options));
-			else
-				this.db.all(options.query, this.done.bind(this, options));
-		},
-
-		//ent, field
-		count: function (options) {
-			var key = options.ent;
-			var table = options.field;
-
-			var query = `SELECT COUNT(*) FROM ${table}`;
-
-			var filter = options.filter;
-			if (filter) {
-				query += ` WHERE`;
-
-				filter.forEach(function (f) {
-					query += ` ${f.field} ${f.operator} ${value}`;
-				});
-			} else {
-				query += ` WHERE key = '${key}'`;
-			}
-
-			query += ` LIMIT ${options.limit || 1}`;
-
-			var offset = options.offset;
-			if (offset)
-				query += ` OFFSET ${offset}`;
-
-			options.query = query;
+			options.query = `SELECT * FROM ${table} WHERE key = '${key}' LIMIT 1`;
 
 			this.db.get(options.query, this.done.bind(this, options));
 		},
@@ -156,20 +78,6 @@ define([
 			var table = options.field;
 
 			var query = `INSERT INTO ${table} (key, value) VALUES('${key}', '${options.value}')`;
-			var extraFields = options.extraFields;
-			if (extraFields) {
-				extraFields.key = key;
-				extraFields.value = options.value;
-
-				var fields = Object.keys(extraFields)
-					.join(',');
-
-				var values = Object.keys(extraFields)
-					.map(p => `'${extraFields[p]}'`)
-					.join(',');
-
-				query = `INSERT INTO ${table} (${fields}) VALUES (${values})`;
-			}
 
 			if (result)
 				query = `UPDATE ${table} SET value = '${options.value}' WHERE key = '${key}'`;
@@ -181,11 +89,6 @@ define([
 			result = result || {
 				value: null
 			};
-			if (!result.hasOwnProperty('value')) {
-				result = {
-					value: result
-				};
-			}
 
 			if (options.callback)
 				options.callback(result.value);
