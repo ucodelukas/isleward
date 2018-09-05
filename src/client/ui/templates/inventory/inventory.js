8cdd0c94ecf7f2179c427034fd402acbd108b9e5
BigBadWaffle <skichenbrand@gmail.com>
2018-09-05 09:35:51 +0200

    fixes #803

--- src/client/ui/templates/inventory/inventory.js
+++ src/client/ui/templates/inventory/inventory.js
@@ -114,16 +114,19 @@ define([
 					.css('background', 'url(' + spritesheet + ') ' + imgX + 'px ' + imgY + 'px')
 					.on('contextmenu', this.showContext.bind(this, item));
 
-				if (item.quantity > 1)
-					itemEl.find('.quantity').html(item.quantity);
-				else if (item.eq)
-					itemEl.find('.quantity').html('EQ');
-				else if (item.active)
-					itemEl.find('.quantity').html('EQ');
+				if (item.quantity > 1 || item.eq || item.active || item.has('quickSlot')) {
+					let elQuantity = itemEl.find('.quantity');
+					let txtQuantity = item.quantity;
+					if (!txtQuantity)
+						txtQuantity = item.has('quickSlot') ? 'QS' : 'EQ';
 
-				if (item.eq)
-					itemEl.addClass('eq');
-				else if (item.isNew) {
+					elQuantity.html(txtQuantity);
+
+					//If the item doesn't have a quantity and we reach this point
+					//it must mean that it's active, EQd or QSd
+					if (!item.quantity)
+						itemEl.addClass('eq');
+				} else if (item.isNew) {
 					itemEl.addClass('new');
 					itemEl.find('.quantity').html('NEW');
 				}
