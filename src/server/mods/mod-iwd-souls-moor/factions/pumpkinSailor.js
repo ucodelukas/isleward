f86eee3a6067046a9a6474779aa388839cf0ec4f
Big Bad Waffle <skichenbrand@gmail.com>
2018-09-09 08:50:23 +0200

    renamed iwd mod folders

--- src/server/mods/mod-iwd-souls-moor/factions/pumpkinSailor.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	id: 'pumpkinSailor',
-	name: 'The Pumpkin Sailor',
-	description: 'The Pumpkin Sailor scours the seas in search of lost souls that were doomed to roam the open waters alone.',
-
-	rewards: {
-
-	}
-};
