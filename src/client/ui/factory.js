5853a5aadde093ef4e19243dbb10e7cd68f161b2
BigBadWaffle <skichenbrand@gmail.com>
2018-09-12 16:13:12 +0200

    cleanup

--- src/client/ui/factory.js
+++ src/client/ui/factory.js
@@ -72,7 +72,7 @@ define([
 			require([this.root + 'ui/templates/' + type + '/' + type], this.onGetTemplate.bind(this, options));
 		},
 		onGetTemplate: function (options, template) {
-			let ui = _.create(uiBase, template);
+			let ui = $.extend(true, {}, uiBase, template);
 			ui.setOptions(options);
 			ui.render();
 			ui.el.data('ui', ui);
