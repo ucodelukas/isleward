7dc02689f6566fc5ba6f8e2284fe0310dc4923bb
Big Bad Waffle <skichenbrand@gmail.com>
2018-10-03 20:01:54 +0200

    fixes #869

--- src/server/config/maps/cave/quests.js
+++ src/server/config/maps/cave/quests.js
@@ -1,3 +1,13 @@
 module.exports = {
-	infini: []
+	infini: [{
+		name: 'Purveyor of Artefacts',
+		type: 'loot',
+		subType: 'slot',
+		quantity: 1
+	}, {
+		name: 'The Culling',
+		type: 'killX',
+		subType: 'mobType',
+		quantity: [5, 10]
+	}]
 };
