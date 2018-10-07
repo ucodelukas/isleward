7c9f91c46381aed7aa114db2d33f6f80843a0d98
Big Bad Waffle <skichenbrand@gmail.com>
2018-10-07 20:10:26 +0200

    temp fix to stop crashes

--- src/server/world/syncer.js
+++ src/server/world/syncer.js
@@ -73,8 +73,8 @@ module.exports = {
 					let py = p.y;
 
 					let canSee = true;
-					if (!o.width)
-						canSee = (Math.abs(ox - px) <= 25 && Math.abs(oy - py) < 14);
+					/*if (!o.width)
+						canSee = (Math.abs(ox - px) <= 25 && Math.abs(oy - py) < 14);*/
 
 					let hasSeen = p.player.hasSeen(oId);
 
