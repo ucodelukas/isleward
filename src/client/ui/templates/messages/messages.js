9f1e984a1ef06bbf3ddafae6a861ff6da9c8622d
Big Bad Waffle <skichenbrand@gmail.com>
2018-10-14 13:09:35 +0200

    another fix for #899

--- src/client/ui/templates/messages/messages.js
+++ src/client/ui/templates/messages/messages.js
@@ -158,13 +158,14 @@ define([
 
 			messages.forEach(m => {
 				let message = m.message;
-				let source = message.split(':') + ': ';
 
 				if (this.blockedPlayers.includes(m.source))
 					return;
 
-				if (m.item)
-					message = source + '<span class="q' + (m.item.quality || 0) + '">' + message.replace(source, '') + '</span>';
+				if (m.item) {
+					let source = message.split(':')[0];
+					message = source + ': <span class="q' + (m.item.quality || 0) + '">' + message.replace(source + ': ', '') + '</span>';
+				}
 
 				let el = $('<div class="list-message ' + m.class + '">' + message + '</div>')
 					.appendTo(container);
