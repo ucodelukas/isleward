d50415e3173c0196bf903c84a8df591c3de5352d
Big Bad Waffle <skichenbrand@gmail.com>
2018-10-18 18:23:55 +0000

    Revert "fixes #87"
    This reverts commit c28c1f4a5de5616ac604eb4421d0dabcc2bee60a

--- src/client/ui/templates/tooltipItem/tooltipItem.js
+++ src/client/ui/templates/tooltipItem/tooltipItem.js
@@ -343,17 +343,10 @@ define([
 					.show();
 				tooltip.find('.damage')
 					.show();
-				if (item.spell.manaCost) {
-					tooltip.find('.spellCost')
-						.html(item.spell.manaCost + ' mana')
-						.show();
-				}
 				if (item.ability)
 					tooltip.find('.spellName').hide();
-			} else {
+			} else
 				tooltip.find('.spellName').hide();
-				tooltip.find('.spellCost').hide();
-			}
 
 			tooltip.find('.worth').html(item.worthText ? ('<br />value: ' + item.worthText) : '');
 
