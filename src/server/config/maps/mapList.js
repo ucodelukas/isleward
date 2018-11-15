e5840066d9438e386b4205c158226887a04f631f
notme <caseypinit@gmail.com>
2018-11-14 16:54:28 -0800

    expose the map list to mods

--- src/server/config/maps/mapList.js
+++ src/server/config/maps/mapList.js
@@ -1,7 +1,16 @@
-module.exports = [
+let events = require('../../misc/events');
+
+let config = [
 	'cave',
 	'estuary',
 	'sewer',
 	'fjolarok',
 	'dungeon'
 ];
+
+module.exports = {
+	init: function () {
+		events.emit('onBeforeGetMapList', config);
+		this.mapList = config;
+	}
+};
