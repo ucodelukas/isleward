2e289cf75017f406095a262022e869c22291ab1e
Shaun Kichenbrand <shaunkich@intenda.net>
2019-06-02 14:40:17 +0200

    fixes #1101

--- src/server/config/effects/effectLifeDrain.js
+++ src/server/config/effects/effectLifeDrain.js
@@ -1,11 +1,24 @@
+let combat = require('../../combat/combat');
+
 module.exports = {
 	type: 'lifeDrain',
 
 	events: {
 		afterTick: function () {
-			this.obj.stats.takeDamage({
-				amount: this.amount
-			}, 1, this.caster);
+			let newDamage = combat.getDamage({
+				source: {
+					stats: {
+						values: {}
+					}
+				},
+				isAttack: false,
+				target: this.obj,
+				damage: this.amount,
+				element: this.element,
+				noCrit: true
+			});
+				
+			this.obj.stats.takeDamage(newDamage, 1, this.caster);
 		}
 	}
 };
