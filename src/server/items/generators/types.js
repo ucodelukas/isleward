821907936bec11c4c94913905f229dd263051642
Shaun Kichenbrand <shaunk@intenda.net>
2019-10-31 19:53:15 +0200

    fixes #1252

--- src/server/items/generators/types.js
+++ src/server/items/generators/types.js
@@ -3,7 +3,34 @@ let armorMaterials = require('../config/armorMaterials');
 
 module.exports = {
 	generate: function (item, blueprint) {
-		let type = blueprint.type || _.randomKey(configTypes.types[item.slot]);
+		let type = blueprint.type;
+
+		if (!type) {
+			//Pick a material type first
+			const types = configTypes.types[item.slot];
+			const typeArray = Object.entries(types);
+			const materials = Object.values(types)
+				.map(t => {
+					return t.material;
+				})
+				.filter((m, i) => i === typeArray.findIndex(t => t[1].material === m));
+			console.log(item.slot, materials);
+
+			const material = materials[~~(Math.random() * materials.length)];
+
+			const possibleTypes = {};
+
+			Object.entries(types)
+				.forEach(t => {
+					const [ typeName, typeConfig ] = t;
+
+					if (typeConfig.material === material)
+						possibleTypes[typeName] = typeConfig;
+				});
+
+			type = _.randomKey(possibleTypes);
+		}
+
 		let typeBlueprint = configTypes.types[item.slot][type] || {};
 
 		if (!typeBlueprint)
