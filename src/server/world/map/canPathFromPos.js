4a4cefbc1ca837e8cee652f27fed09437b1f6e1c
Shaun Kichenbrand <shaunk@intenda.net>
2020-01-02 07:04:47 +0200

    fixes #1331

---
+++ src/server/world/map/canPathFromPos.js
@@ -0,0 +1,37 @@
+let objects = require('../../objects/objects');
+let physics = require('../physics');
+
+const canPath = (pos, positions, maxDistance = 0) => {
+	return positions.some(p => {
+		const path = physics.getPath(pos, p);
+		//Are we on the position?
+		if (!path.length)
+			return true;
+
+		const { x, y } = path[path.length - 1];
+		//Can we get close enough to the position?
+		const isCloseEnough = Math.max(Math.abs(p.x - x), Math.abs(p.y - y)) <= maxDistance;
+		if (isCloseEnough)
+			return true;
+
+		return false;
+	});
+};
+
+module.exports = (map, pos) => {
+	const canPathToSpawn = canPath(pos, map.spawn);
+
+	if (canPathToSpawn)
+		return true;
+
+	const portals = objects.objects.filter(o => o.portal);
+	const canPathToPortal = canPath(pos, portals);
+
+	if (canPathToPortal)
+		return true;
+
+	const doors = objects.objects.filter(o => o.door);
+	const canPathToDoor = canPath(pos, doors, 1);
+
+	return canPathToDoor;
+};
