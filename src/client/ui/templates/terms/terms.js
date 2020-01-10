29f73cf0c57fd17124f3e29d3e447cb17b037eb7
Vildravn <vildravn@gmail.com>
2020-01-11 00:29:12 +0100

    Fix whitespaces in terms.js

--- src/client/ui/templates/terms/terms.js
+++ src/client/ui/templates/terms/terms.js
@@ -1,25 +1,25 @@
 define([
-	'js/system/events',
-	'js/system/client',
-	'ui/factory',
-	'html!ui/templates/terms/template',
-	'css!ui/templates/terms/styles',
+    'js/system/events',
+    'js/system/client',
+    'ui/factory',
+    'html!ui/templates/terms/template',
+    'css!ui/templates/terms/styles',
     'js/rendering/renderer',
     'js/config'
 ], function (
-	events,
-	client,
-	uiFactory,
-	template,
-	styles,
+    events,
+    client,
+    uiFactory,
+    template,
+    styles,
     renderer,
     config
 ) {
-	return {
-		tpl: template,
+    return {
+        tpl: template,
         centered: true,
 
-		postRender: function () {
+        postRender: function () {
             this.tryAutoAccept();
 
             this.find('.btnCancel').on('click', this.cancel.bind(this));
@@ -30,15 +30,15 @@ define([
             if (config.readTos)
                 this.accept();
         },
-        
+
         cancel: function () {
-			window.location = window.location;
+            window.location = window.location;
         },
-        
+
         accept: function () {
             config.set('readTos', true);
             this.destroy();
             uiFactory.build('characters', {});
         }
-	};
+    };
 });
