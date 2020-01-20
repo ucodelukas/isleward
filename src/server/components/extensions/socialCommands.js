f3f482a1c0a153f184b65315e4a33921993b4739
Peyrille Benjamin <peyrille.benjamin@gmail.com>
2020-01-20 01:22:59 +0100

    Fix #1365

--- src/server/components/extensions/socialCommands.js
+++ src/server/components/extensions/socialCommands.js
@@ -441,6 +441,10 @@ module.exports = {
 	},
 
 	getItem: function (config) {
+		if (typeof config !== 'object') {
+			return;
+		}
+
 		if (config.slot === 'set') {
 			configSlots.slots.forEach(function (s) {
 				if (s === 'tool')
