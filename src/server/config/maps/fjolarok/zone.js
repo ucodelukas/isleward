444d1ff82498a99fb569b9f18c6113d597e43f02
Shaun <skichenbrand@gmail.com>
2020-04-11 14:41:21 +0200

    asvald runes are now always commong

--- src/server/config/maps/fjolarok/zone.js
+++ src/server/config/maps/fjolarok/zone.js
@@ -854,56 +854,56 @@ module.exports = {
 						extra: [{
 							generate: true,
 							spell: true,
-							spellQuality: 0,
+							quality: 0,
 							infinite: true,
 							spellName: 'magic missile',
 							worth: 3
 						}, {
 							generate: true,
 							spell: true,
-							spellQuality: 0,
+							quality: 0,
 							infinite: true,
 							spellName: 'ice spear',
 							worth: 3
 						}, {
 							generate: true,
 							spell: true,
-							spellQuality: 0,
+							quality: 0,
 							infinite: true,
 							spellName: 'smite',
 							worth: 3
 						}, {
 							generate: true,
 							spell: true,
-							spellQuality: 0,
+							quality: 0,
 							infinite: true,
 							spellName: 'consecrate',
 							worth: 3
 						}, {
 							generate: true,
 							spell: true,
-							spellQuality: 0,
+							quality: 0,
 							infinite: true,
 							spellName: 'slash',
 							worth: 3
 						}, {
 							generate: true,
 							spell: true,
-							spellQuality: 0,
+							quality: 0,
 							infinite: true,
 							spellName: 'charge',
 							worth: 3
 						}, {
 							generate: true,
 							spell: true,
-							spellQuality: 0,
+							quality: 0,
 							infinite: true,
 							spellName: 'flurry',
 							worth: 3
 						}, {
 							generate: true,
 							spell: true,
-							spellQuality: 0,
+							quality: 0,
 							infinite: true,
 							spellName: 'smokebomb',
 							worth: 3
