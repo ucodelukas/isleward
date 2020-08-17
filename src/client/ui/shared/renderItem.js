04f477ba2fd268d46989d595bfa69c64229b3950
Shaun <skichenbrand@gmail.com>
2020-08-17 07:27:57 +0200

    fixes #1509

--- src/client/ui/shared/renderItem.js
+++ src/client/ui/shared/renderItem.js
@@ -51,6 +51,8 @@ define([
 			hideTooltip(null, item);
 
 		const contextConfig = getItemContextConfig(item);
+		if (!contextConfig.length)
+			return;
 
 		events.emit('onContextMenu', contextConfig, e);
 
