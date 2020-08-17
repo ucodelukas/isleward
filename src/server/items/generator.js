31d9f69626d949c099fc40d684293a191c183a1a
Shaun <skichenbrand@gmail.com>
2020-08-17 07:57:52 +0200

    fixes #1520

--- src/server/items/generator.js
+++ src/server/items/generator.js
@@ -53,7 +53,7 @@ module.exports = {
 		if (blueprint.noCurrency)
 			currencyChance = 0;
 
-		if (!blueprint.slot && !blueprint.noSpell && !blueprint.material) {
+		if (!blueprint.slot && !blueprint.noSpell && !blueprint.material && !blueprint.type) {
 			isSpell = blueprint.spell;
 			isCurrency = blueprint.currency;
 			if ((!isCurrency) && (!isSpell) && ((!hadBlueprint) || ((!blueprint.type) && (!blueprint.slot) && (!blueprint.stats)))) {
@@ -79,6 +79,7 @@ module.exports = {
 			item.quality = blueprint.quality || 0;
 			materialGenerators.forEach(g => g.generate(item, blueprint));
 		} else if (blueprint.type === 'mtx') {
+			//TODO: MTXs have been moved to a mod so we shouldn't have this any more
 			item = extend({}, blueprint);
 			delete item.chance;
 		} else if (blueprint.type === 'recipe') 
