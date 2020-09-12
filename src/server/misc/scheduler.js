bacc3da47321dd84dfb76676618c78f1ea10f11f
Shaun <skichenbrand@gmail.com>
2020-09-12 14:58:01 +0000

    fixes #1560
    (cherry picked from commit d3230bbc38641d38b3fd4299bae03d06aed5a0f9)

--- src/server/misc/scheduler.js
+++ src/server/misc/scheduler.js
@@ -27,8 +27,13 @@ module.exports = {
 
 			if (f.length === 1) {
 				f = f[0].split('/');
-				if (f.length === 1)
-					return (useTime === ~~f[0]);
+				if (f.length === 1) {
+					const options = f[0].split(',');
+					const isOk = options.some(o => {
+						return ~~o === useTime;
+					});
+					return isOk;
+				}
 				return ((useTime % f[1]) === 0);
 			}
 			return ((useTime >= f[0]) && (useTime <= f[1]));
