ucodelukas/isleward

commit 09a9c708110fb3d27e2d998664727f133fd099f8

another fix for #1564 and fixed a rare crash that was introduced a year ago by 9053b1335a8f1b0069f6faebeb7859349dd8107b

src/server/config/spells/spellTemplate.js
@@ -64,7 +64,7 @@ module.exports = {
 	updateBase: function () {
 		if (this.castTime > 0) {
 			let action = this.currentAction;
-			if (_.getDeepProperty(action, 'target.destroyed') || !this.canCast(action.target)) {
+			if (_.getDeepProperty(action, 'target.destroyed') || (action && !this.canCast(action.target))) {
 				this.currentAction = null;
 				this.castTime = 0;
 				this.obj.syncer.set(false, null, 'casting', 0);

src/server/events/events.js
@@ -254,7 +254,7 @@ module.exports = {
 	stopAll: function () {
 		this.configs.forEach(c => {
 			if (c.event)
-				this.stopEvent(c);
+				c.event.done = true;
 		});
 	},
 

commit 598c507c66150ba20789d2b1ecb2a7f38a38ed01

fixed a bug introduced in 09a9c708110fb3d27e2d998664727f133fd099f8 this is the right fix now...I think

src/server/config/spells/spellTemplate.js
@@ -64,7 +64,7 @@ module.exports = {
 	updateBase: function () {
 		if (this.castTime > 0) {
 			let action = this.currentAction;
-			if (_.getDeepProperty(action, 'target.destroyed') || (action && !this.canCast(action.target))) {
+			if (action && (_.getDeepProperty(action, 'target.destroyed') || !this.canCast(action.target))) {
 				this.currentAction = null;
 				this.castTime = 0;
 				this.obj.syncer.set(false, null, 'casting', 0);